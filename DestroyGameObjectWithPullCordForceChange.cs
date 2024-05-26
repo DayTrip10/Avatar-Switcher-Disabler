d8628f4dc4225e5a60fa2cd94ea40cf12bc0b6ab
DayTrip10 <136037341+DayTrip10@users.noreply.github.com>
2024-05-26 04:45:57 -0400

    Stuff
    Attempt

--- DestroyGameObjectWithPullCordForceChange.cs
+++ DestroyGameObjectWithPullCordForceChange.cs
@@ -3,27 +3,30 @@ using UnityEngine;
 using SLZ.Props;
 using MelonLoader;
 
-public class DestroyGameObjectWithPullCordForceChange : MelonMod
+namespace bonelab_template
 {
-    private float checkInterval = 5.0f; // Check every 5 seconds
-    private float nextCheckTime = 0.0f;
-
-    public override void OnApplicationStart()
+    public class DestroyGameObjectWithPullCordForceChange : MelonMod
     {
-        MelonLogger.Msg("DestroyGameObjectWithPullCordForceChange mod loaded");
-    }
+        private float checkInterval = 5.0f; // Check every 5 seconds
+        private float nextCheckTime = 0.0f;
 
-    public override void OnUpdate()
-    {
-        if (Time.time >= nextCheckTime)
+        public override void OnApplicationStart()
         {
-            nextCheckTime = Time.time + checkInterval;
-            PullCordForceChange[] pullCordForceChanges = GameObject.FindObjectsOfType<PullCordForceChange>();
-            foreach (PullCordForceChange pullCordForceChange in pullCordForceChanges)
+            MelonLogger.Msg("DestroyGameObjectWithPullCordForceChange mod loaded");
+        }
+
+        public override void OnUpdate()
+        {
+            if (Time.time >= nextCheckTime)
             {
-                string objectName = pullCordForceChange.gameObject.name;
-                GameObject.Destroy(pullCordForceChange.gameObject);
-                MelonLogger.Msg($"Found and destroyed GameObject: {objectName}");
+                nextCheckTime = Time.time + checkInterval;
+                PullCordForceChange[] pullCordForceChanges = GameObject.FindObjectsOfType<PullCordForceChange>();
+                foreach (PullCordForceChange pullCordForceChange in pullCordForceChanges)
+                {
+                    string objectName = pullCordForceChange.gameObject.name;
+                    GameObject.Destroy(pullCordForceChange.gameObject);
+                    MelonLogger.Msg($"Found and destroyed GameObject: {objectName}");
+                }
             }
         }
     }
