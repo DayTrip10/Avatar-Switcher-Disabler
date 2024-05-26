1c71d4a36e7a898ba87a3197343998a7890cfeed
DayTrip10 <136037341+DayTrip10@users.noreply.github.com>
2024-05-26 05:51:27 -0400

    Update DestroyGameObjectWithPullCordForceChange.cs

--- DestroyGameObjectWithPullCordForceChange.cs
+++ DestroyGameObjectWithPullCordForceChange.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using MelonLoader;
 using HarmonyLib;
 using SLZ.Props;
@@ -8,22 +7,26 @@ namespace MyGameMods
 {
     public class Daytrip : MelonMod
     {
-        private HarmonyLib.Harmony harmony;
-
         public override void OnApplicationStart()
         {
             MelonLogger.Msg("Daytrip mod loaded");
-            harmony = new HarmonyLib.Harmony("com.daytrip.DestroyGameObjectWithPullCordForceChange");
-            harmony.PatchAll(typeof(Daytrip).Assembly);
+            var harmony = new Harmony("com.daytrip.DestroyGameObjectWithPullCordForceChange");
+            harmony.PatchAll();
         }
 
-        // No need for OnUpdate if using Harmony for patching
+        public override void OnApplicationQuit()
+        {
+            var harmony = new Harmony("com.daytrip.DestroyGameObjectWithPullCordForceChange");
+            harmony.UnpatchAll();
+        }
     }
 
-    [HarmonyPatch(typeof(UnityEngine.Object), "Instantiate", typeof(UnityEngine.Object), typeof(Vector3), typeof(Quaternion), typeof(Transform))]
-    public static class InstantiatePatch
+    [HarmonyPatch(typeof(UnityEngine.Object))]
+    public class InstantiatePatch
     {
-        static void Prefix(ref UnityEngine.Object original, ref Vector3 position, ref Quaternion rotation, Transform parent)
+        [HarmonyPrefix]
+        [HarmonyPatch("Instantiate", typeof(UnityEngine.Object), typeof(Vector3), typeof(Quaternion), typeof(Transform))]
+        public static void Prefix(ref UnityEngine.Object original, ref Vector3 position, ref Quaternion rotation, Transform parent)
         {
             // Check if the object being instantiated has the PullCordForceChange component
             if (original is GameObject originalGameObject)
