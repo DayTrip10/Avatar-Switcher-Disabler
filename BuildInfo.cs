bcaf3e176ec2a5bf865e271075faaddd8210b276
DayTrip10 <136037341+DayTrip10@users.noreply.github.com>
2024-05-26 04:27:42 -0400

    Added Stuff
    Stuff

---
+++ BuildInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace bonelab_template
+{
+    internal partial class Main
+    {
+        internal const string Name = "Avatar Switcher Disabler";
+        internal const string Description = "Tool to stop forced avatar changes";
+        internal const string Author = "DayTrip10";
+        internal const string Company = "Mod Company";
+        internal const string Version = "1.0.0";
+        internal const string DownloadLink = "Link";
+    }
+}
---
+++ DestroyGameObjectWithPullCordForceChange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using SLZ.Props;
+using MelonLoader;
+
+public class DestroyGameObjectWithPullCordForceChange : MelonMod
+{
+    private float checkInterval = 5.0f; // Check every 5 seconds
+    private float nextCheckTime = 0.0f;
+
+    public override void OnApplicationStart()
+    {
+        MelonLogger.Msg("DestroyGameObjectWithPullCordForceChange mod loaded");
+    }
+
+    public override void OnUpdate()
+    {
+        if (Time.time >= nextCheckTime)
+        {
+            nextCheckTime = Time.time + checkInterval;
+            PullCordForceChange[] pullCordForceChanges = GameObject.FindObjectsOfType<PullCordForceChange>();
+            foreach (PullCordForceChange pullCordForceChange in pullCordForceChanges)
+            {
+                string objectName = pullCordForceChange.gameObject.name;
+                GameObject.Destroy(pullCordForceChange.gameObject);
+                MelonLogger.Msg($"Found and destroyed GameObject: {objectName}");
+            }
+        }
+    }
+}
