53cebbda05e029054d0fb69f61f0beb093c02990
DayTrip10 <136037341+DayTrip10@users.noreply.github.com>
2024-05-26 23:27:54 -0400

    aa

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -3,7 +3,7 @@
     internal partial class Main
     {
         internal const string Name = "Avatar Switcher Disabler";
-        internal const string Description = "Tool to stop forced avatar changes";
+        internal const string Description = "A code mod that prevents pesky forced avatar changes, mostly for BONELAB Fusion";
         internal const string Author = "DayTrip10";
         internal const string Company = "Mod Company";
         internal const string Version = "1.0.0";
